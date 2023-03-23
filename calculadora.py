5d27dc4a35db0ffbe0eb8e1278344a141b0df3d4
unknown <marianasevarollisenai@gmail.com>
2023-03-23 11:41:54 -0300

    Neste arquivo foi criado uma calculadora de conversão de unidades de armazenamento em Python. Inicialmente, criei um repositório no GitHub, clonei e criei uma branch. Após isso, escrevi o programa citado pelo professor para iniciar a programação. Usei funções para converter todos os valores de unidades de armazenamento, usando parâmetros com base input e convertendo e retornando-as para float. Por fim, criei uma constante com o valor de 1024 para usa-lá nas funções que precisavam desse valor e testei algumas vezes.

---
+++ calculadora.py
@@ -0,0 +1,70 @@
+valorpadrão=1024
+def converterStringParaFloat(value):
+    print('Valor convertido de str para float')
+    return float(value)
+
+def bitParaByte(valorASerConvertido):
+    print('Valor convertido de bit para byte')
+    bytesCalculado = valorASerConvertido / 8
+    return bytesCalculado
+
+def byteParaBit(valorASerConvertido):
+    print('Valor convertido de byte para bit')
+    bitsCalculado = valorASerConvertido * 8
+    return bitsCalculado
+
+def byteParaKB(valorASerConvertido):
+    print('Valor convertido de byte para KB')
+    KBCalculado = valorASerConvertido / valorpadrão
+    return KBCalculado
+
+def KBParaByte(valorASerConvertido):
+    print('Valor convertido de KB para byte')
+    ByteCalculado = valorASerConvertido * valorpadrão
+    return ByteCalculado
+
+def KBParaMB(valorASerConvertido):
+    print('Valor convertido de KB para MB')
+    MBCalculado = valorASerConvertido / valorpadrão
+    return MBCalculado
+
+def MBParaKB(valorASerConvertido):
+    print('Valor convertido de MB para KB')
+    KBCalculado = valorASerConvertido * valorpadrão
+    return KBCalculado
+
+def MBParaGB(valorASerConvertido):
+    print('Valor convertido de MB para GB')
+    GBCalculado = valorASerConvertido / valorpadrão
+    return GBCalculado
+
+def GBParaMB(valorASerConvertido):
+    print('Valor convertido de GB para MB')
+    MBCalculado = valorASerConvertido * valorpadrão
+    return MBCalculado
+
+def GBParaTB(valorASerConvertido):
+    print('Valor convertido de GB para TB')
+    TBCalculado = valorASerConvertido / valorpadrão
+    return TBCalculado
+
+def TBParaGB(valorASerConvertido):
+    print('Valor convertido de TB para GB')
+    GBCalculado = valorASerConvertido * valorpadrão
+    return GBCalculado
+
+def TBParaPB(valorASerConvertido):
+    print('Valor convertido de TB para PB')
+    PBCalculado = valorASerConvertido / valorpadrão
+    return PBCalculado
+
+def PBParaTB(valorASerConvertido):
+    print('Valor convertido de PB para TB')
+    TBCalculado = valorASerConvertido * valorpadrão
+    return TBCalculado
+
+
+print('Insira o valor a ser convertido')
+entradaDoTecladoValorASerConvertido  = converterStringParaFloat(input())
+valorConvertido = PBParaTB(entradaDoTecladoValorASerConvertido)
+print(valorConvertido)
