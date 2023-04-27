196fb9c7c56ebb6ffe6cefa6f0d3d84480808d51
unknown <marianasevarollisenai@gmail.com>
2023-04-27 16:02:44 -0300

    Inicialmente, fiz uma lista que contém todas as unidades de medida de armazenamento, as funções para receber quais unidades serão escolhidas como inicial e final e para receber o valor a ser convertido, assim como as variáveis posições inicial e final e a unidade bit (que tem um valor diferente de conversão, que é 8), para me ajudar posteriormente a fazer o código.

---
+++ otimizacao-de-códigos.py
@@ -0,0 +1,7 @@
+unidades = ['bit','byte','KB','MB','GB','TB','PB']
+unidadeInicial = input(f"Insira a unidade inicial: ")
+unidadeFinal = input(f"Insira a unidade final: ")
+valor = int(input(f"Insira o valor a ser convertido: "))
+posiçãoinicial = 0
+posiçãofinal = 0
+unidadebit = 'bit'
